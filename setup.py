c96f3e2c97739e0771b6dc4450febddde20353f9
Nate Atkinson <njatkinson@users.noreply.github.com>
2017-06-26 12:13:38 -0500

    * tweak setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name = 'shitty_tools',
     packages = ['shitty_tools', 'shitty_tools.key_value', 'shitty_tools.rpc'],
-    version = '2017.06.26.0',
-    description = 'A collection of Python modules including tools for concurrency, connection pools, key-value storage'
-                  ' and rate limiting.',
+    version = '2017.06.26.1',
+    description = 'A collection of Python modules including tools for connection pools, entity-attribute-value tables, '
+                  'key-value storage, and rate limiting.',
     author = 'Nate Atkinson',
     url = 'https://github.com/njatkinson/shitty_tools',
-    download_url = 'https://github.com/njatkinson/shitty_tools/archive/2017.06.26.0.tar.gz',
-    keywords = ['connection pool', 'key value', 'rate limit', 'sqlalchemy']
+    download_url = 'https://github.com/njatkinson/shitty_tools/archive/2017.06.26.1.tar.gz',
+    keywords = ['connection pool', 'key value', 'rate limit', 'sqlalchemy', 'entity attribute value']
 )
